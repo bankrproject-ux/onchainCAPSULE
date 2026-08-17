37356c18f6a01e3723257d756e20536c6a8c6756
bankrproject-ux <bankrprojek@gmail.com>
2026-08-18 01:50:53 +0700

    Update CapsuleNFT.sol

--- contracts/CapsuleNFT.sol
+++ contracts/CapsuleNFT.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CapsuleNFT is ERC721, ERC721Burnable, Ownable {
+contract CapsuleNFT is ERC721, Ownable {
     uint256 public constant MAX_SUPPLY = 5000;
     uint256 public constant MAX_PER_WALLET = 2;
 
@@ -16,35 +15,80 @@ contract CapsuleNFT is ERC721, ERC721Burnable, Ownable {
 
     mapping(address => uint256) public mintedPerWallet;
 
+    address public vault;
+
+    error InvalidVault();
+    error InvalidQuantity();
+    error WalletMintLimitExceeded();
+    error MaxSupplyExceeded();
+    error NotVault();
+    error TokenDoesNotExist();
+
     constructor(
         string memory baseTokenURI_
-    ) ERC721("onchainCAPSULE", "CAPSULE") Ownable(msg.sender) {
+    )
+        ERC721("onchainCAPSULE", "CAPSULE")
+        Ownable(msg.sender)
+    {
         _baseTokenURI = baseTokenURI_;
     }
 
     function mint(uint256 quantity) external {
-        require(quantity > 0, "Quantity must be greater than 0");
+        if (quantity == 0) {
+            revert InvalidQuantity();
+        }
 
-        require(
-            mintedPerWallet[msg.sender] + quantity <= MAX_PER_WALLET,
-            "Wallet mint limit exceeded"
-        );
+        if (
+            mintedPerWallet[msg.sender] + quantity >
+            MAX_PER_WALLET
+        ) {
+            revert WalletMintLimitExceeded();
+        }
 
-        require(
-            totalMinted + quantity <= MAX_SUPPLY,
-            "Max supply exceeded"
-        );
+        if (
+            totalMinted + quantity >
+            MAX_SUPPLY
+        ) {
+            revert MaxSupplyExceeded();
+        }
 
         mintedPerWallet[msg.sender] += quantity;
 
         for (uint256 i = 0; i < quantity; i++) {
             uint256 tokenId = _nextTokenId;
 
-            _safeMint(msg.sender, tokenId);
+            _safeMint(
+                msg.sender,
+                tokenId
+            );
 
             _nextTokenId++;
             totalMinted++;
         }
+    }
+
+    function setVault(
+        address vault_
+    ) external onlyOwner {
+        if (vault_ == address(0)) {
+            revert InvalidVault();
+        }
+
+        vault = vault_;
+    }
+
+    function vaultBurn(
+        uint256 tokenId
+    ) external {
+        if (msg.sender != vault) {
+            revert NotVault();
+        }
+
+        if (!_ownerOf(tokenId).exists()) {
+            revert TokenDoesNotExist();
+        }
+
+        _burn(tokenId);
     }
 
     function setBaseTokenURI(
@@ -62,7 +106,11 @@ contract CapsuleNFT is ERC721, ERC721Burnable, Ownable {
         return _baseTokenURI;
     }
 
-    function remainingSupply() external view returns (uint256) {
+    function remainingSupply()
+        external
+        view
+        returns (uint256)
+    {
         return MAX_SUPPLY - totalMinted;
     }
 }
